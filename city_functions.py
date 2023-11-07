ac8fda7dded48ba47af0c4725923eb9977fc6b05
Dylan-Golden <143642079+Dylan-Golden@users.noreply.github.com>
2023-11-06 21:36:55 -0500

    Delete city_functions.py

--- city_functions.py
+++
@@ -1,6 +0,0 @@
-#try it 11-1
-def thing(city, country, population=None):
-    if population == None:
-        return (f"{city}, {country}")
-    else:
-        return (f"{city}, {country}, population - {population}") 
